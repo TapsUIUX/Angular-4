3f85a5b99b85860f170cbf57449427114bc9dd5b
TapsUIUX <25495356+TapsUIUX@users.noreply.github.com>
2017-10-30 01:35:33 -0400

    Create form.service.ts

---
+++ ReactiveFormValidation/form.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core' ;
+import {UserInterface} from './interface' ;
+
+
+@Injectable() // angular suggests to keep it even you dnt have any dependency.
+export class DataService {    
+getData(): UserInterface[]{
+return [
+    {userName : 'hari'},
+    {userName : 'sam'},
+    {userName : 'JESUS'}
+];
+} 
+    
+}
